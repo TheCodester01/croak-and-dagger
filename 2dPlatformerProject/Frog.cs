b5e4eaee652702b76cb6d36daa7c162ce1fc70ed
peatreat <peatreat123@gmail.com>
2026-03-17 23:15:05 -0500

    Frog Character and Ruins Level

---
+++ 2dPlatformerProject/Frog.cs
@@ -0,0 +1,78 @@
+using Godot;
+using System;
+
+public partial class Frog : CharacterBody2D
+{
+	[Export]
+	public float JumpPower = 0.0f;
+	
+	[Export]
+	public float JumpPowerMinimum = 0.3f;
+	
+	[Export]
+	public int JumpPowerMaxSeconds =  1;
+	
+	private float JumpPowerElapsedTime = 0.0f;
+	
+	public const float HorizontalVelocity = 150.0f;
+	public const float JumpVelocity = -800.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+		
+		// Handle jump power
+		if (IsOnFloor()) {
+			velocity.X = 0;
+			
+  			if (Input.IsActionPressed("ui_accept")) {
+				float t = this.JumpPowerElapsedTime / this.JumpPowerMaxSeconds;
+				this.JumpPower = Mathf.Lerp(this.JumpPowerMinimum, 1.0f, t);
+				
+				this.JumpPowerElapsedTime += (float)delta;
+			} else {
+				this.JumpPowerElapsedTime = 0;
+			}
+		}
+		
+		// Handle Jump.
+		if (Input.IsActionJustReleased("ui_accept") && IsOnFloor()) {
+			velocity.Y = JumpVelocity * this.JumpPower;
+			
+			if (Input.IsActionPressed("ui_left")) {
+				velocity.X = -HorizontalVelocity;
+			} else if (Input.IsActionPressed("ui_right")) {
+				velocity.X = HorizontalVelocity;
+			}
+		}
+		
+		AnimatedSprite2D anim_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		
+		if (velocity.Y < 0)
+		{
+			anim_sprite.Animation = "jump";
+		}
+		else if (velocity.Y > 0) {
+			anim_sprite.Animation = "fall";			
+		}
+		else
+		{
+			anim_sprite.Animation = "idle";
+		}
+
+		anim_sprite.FlipH = direction.X < 0.0;
+
+		anim_sprite.Play();
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
--- 2dPlatformerProject/Player.cs
+++ 2dPlatformerProject/Player.cs
@@ -11,20 +11,20 @@ public partial class Player : CharacterBody2D
 	private AnimatedSprite2D anim_sprite;
 	private int facing_dir = 1;
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
+	public override void _UnhandledInput(InputEvent @event)
+	{
 		if (@event.IsActionPressed("jump") && IsOnFloor())
 		{
 			jumped = true;
 		}
-        base._UnhandledInput(@event);
-    }
+		base._UnhandledInput(@event);
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 direction = Input.GetVector("left", "right", "ui_up", "ui_down"); // Get the input direction and handle the movement/deceleration.
-        Vector2 velocity = Velocity;
-        is_sprinting = Input.IsActionPressed("sprint") && IsOnFloor();
+		Vector2 velocity = Velocity;
+		is_sprinting = Input.IsActionPressed("sprint") && IsOnFloor();
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -33,64 +33,64 @@ public partial class Player : CharacterBody2D
 		}
 
 		// If not standing still
-        if (direction != Vector2.Zero)
+		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
 
-            if (is_sprinting) 
+			if (is_sprinting) 
 			{
-                velocity.X *= SprintMultiplier;
+				velocity.X *= SprintMultiplier;
 			}
-        }
+		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-        }
+		}
 
-        if (direction.X != 0)
-        {
-            facing_dir = direction.X > 0 ? 1 : -1;
-        }
+		if (direction.X != 0)
+		{
+			facing_dir = direction.X > 0 ? 1 : -1;
+		}
 
-        // Handle Jump.
-        if (jumped && IsOnFloor())
+		// Handle Jump.
+		if (jumped && IsOnFloor())
 		{
 			velocity.Y = is_sprinting ? JumpVelocity * 1.10f : JumpVelocity;
 			jumped = false;
 		}
 
 
-        Velocity = velocity;
+		Velocity = velocity;
 		MoveAndSlide();
 	}
 
-    public override void _Process(double delta)
-    {
+	public override void _Process(double delta)
+	{
 
 		anim_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-        if (!IsOnFloor())
-        {
-            anim_sprite.Animation = "jump";
+		if (!IsOnFloor())
+		{
+			anim_sprite.Animation = "jump";
 			anim_sprite.FlipH = facing_dir == -1;
 			float offset_x = anim_sprite.FlipH ? -64 : 0;
 			anim_sprite.Offset = new Vector2(offset_x, 0);
-        }
-        else if (Velocity.X != 0.0 && IsOnFloor())
-        {
-            anim_sprite.Animation = is_sprinting ? "run" : "walk";
+		}
+		else if (Velocity.X != 0.0 && IsOnFloor())
+		{
+			anim_sprite.Animation = is_sprinting ? "run" : "walk";
 			anim_sprite.FlipH = facing_dir == -1;
-            float offset_x = anim_sprite.FlipH ? -64 : 0;
-            anim_sprite.Offset = new Vector2(offset_x, 0);
-        }
-        else
-        {
-            anim_sprite.Animation = "idle";
-        }
+			float offset_x = anim_sprite.FlipH ? -64 : 0;
+			anim_sprite.Offset = new Vector2(offset_x, 0);
+		}
+		else
+		{
+			anim_sprite.Animation = "idle";
+		}
 		
 		anim_sprite.Play();
 
-        base._Process(delta);
-    }
+		base._Process(delta);
+	}
 
 }
