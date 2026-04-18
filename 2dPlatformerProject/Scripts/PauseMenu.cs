b6d49378dce583b61a41e063def94da51a2b06a5
michael picchietti <michaelpicchietti@michaels-MacBook-Pro.local>
2026-04-18 12:38:48 -0500

    added music sliders to pause menu

--- 2dPlatformerProject/Scripts/PauseMenu.cs
+++ 2dPlatformerProject/Scripts/PauseMenu.cs
@@ -2,14 +2,22 @@ using Godot;
 
 public partial class PauseMenu : CanvasLayer
 {
+	private HSlider _musicSlider;
+	private HSlider _sfxSlider;
+
 	public override void _Ready()
 	{
 		Hide();
 		ProcessMode = ProcessModeEnum.Always;
 
+		_musicSlider = GetNode<HSlider>("Panel/VBox/MusicSlider");
+		_sfxSlider = GetNode<HSlider>("Panel/VBox/SfxSlider");
+
 		GetNode<Button>("Panel/VBox/ContinueButton").Pressed += OnContinue;
 		GetNode<Button>("Panel/VBox/MainMenuButton").Pressed += OnMainMenu;
 		GetNode<Button>("Panel/VBox/QuitButton").Pressed += OnQuit;
+		_musicSlider.ValueChanged += OnMusicVolumeChanged;
+		_sfxSlider.ValueChanged += OnSfxVolumeChanged;
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
@@ -25,6 +33,8 @@ public partial class PauseMenu : CanvasLayer
 
 	private void OpenPauseMenu()
 	{
+		_musicSlider.Value = GameManager.Instance.MusicVolume;
+		_sfxSlider.Value = GameManager.Instance.SfxVolume;
 		GetTree().Paused = true;
 		Show();
 	}
@@ -45,5 +55,15 @@ public partial class PauseMenu : CanvasLayer
 	private void OnQuit()
 	{
 		GetTree().Quit();
+	}
+
+	private void OnMusicVolumeChanged(double value)
+	{
+		GameManager.Instance.MusicVolume = (float)value;
+	}
+
+	private void OnSfxVolumeChanged(double value)
+	{
+		GameManager.Instance.SfxVolume = (float)value;
 	}
 }
